299b4294a00e3109455f494f589a08cf76a93f11
shubham-goel <sgoel160@gmail.com>
2016-04-02 02:38:53 +0530

    Changed Stack_integer

--- Experiment/Experiment/Stack_integer.vhd
+++ Experiment/Experiment/Stack_integer.vhd
@@ -32,18 +32,18 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Stack_integer is
 	generic (max_size : INTEGER := 1024;
-				int_range_st : INTEGER := -70;
-				int_range_end : INTEGER := 70);
+  				int_range_st : INTEGER := -70;
+  				int_range_end : INTEGER := 70);
 				
 	Port ( clock : in  STD_LOGIC;
-           reset : in  STD_LOGIC;
-           wr_en : in  STD_LOGIC;
-           rd_en : in  STD_LOGIC;
-           pop : in  STD_LOGIC;
-           din : in  INTEGER range int_range_st to int_range_end;
-           dout : out  INTEGER range int_range_st to int_range_end;
-           full : out  STD_LOGIC;
-           empty : out  STD_LOGIC);
+         reset : in  STD_LOGIC;
+         wr_en : in  STD_LOGIC;
+         rd_en : in  STD_LOGIC;
+         pop : in  STD_LOGIC;
+         din : in  INTEGER range int_range_st to int_range_end;
+         dout : out  INTEGER range int_range_st to int_range_end;
+         full : out  STD_LOGIC;
+         empty : out  STD_LOGIC);
 end Stack_integer;
 
 architecture Behavioral of Stack_integer is
@@ -65,60 +65,36 @@ begin
   empty <= empty_signal;
 
 	RESET: process(clock)
-  begin
-    if reset='1' and rising_edge(clock) then
-      data <= (others=>0);
-      curr_size <= 0;
-      data_out <= 0;
-      empty_signal <= '1';
-      full_signal <= '0';
-    end if ;
-  end process;
-
-  PUSH : process(clock, wr_en)
   variable temp : INTEGER := curr_size;
   begin
     if rising_edge(clock) then
-      if wr_en='1' and full_signal='0' then
+      
+      --RESET
+      if reset='1' then 
+        data <= (others=>0);
+        curr_size <= 0;
+        data_out <= 0;
+        empty_signal <= '1';
+        full_signal <= '0';
+      
+      -- PUSH
+      elsif wr_en='1' and full_signal='0' then 
         data(curr_size) <= din;
         curr_size <= curr_size + 1;
         temp := temp + 1;
-      end if ;
-
-      if temp=(max_size-2) then
-        full_signal <= '1';
-        empty_signal <= '0';
-      elsif temp=0 then
-        full_signal <= '0';
-        empty_signal <= '1';
-      else
-        full_signal <= '0';
-        empty_signal <= '0';
-      end if;
-        
-    end if;
-  end process;
-
-  READ : process(clock, rd_en)
-  begin
-    if rising_edge(clock) then
-      if rd_en='1' and empty_signal='0' then
+      
+      --READ
+      elsif rd_en='1' and empty_signal='0' then 
         data_out <= data(curr_size-1)
-      end if ;
-    end if;
-  end process;
-
-  POP : process( clock, pop )
-  variable temp : INTEGER := curr_size;
-  begin
-    if rising_edge(clock) then
-      if pop = '1' and empty_signal='0' then
+      
+      --POP
+      elsif pop = '1' and empty_signal='0' then 
         data_out <= data(curr_size-1)
         curr_size <= curr_size - 1;
         temp := temp - 1;
       end if;
 
-      if temp=(max_size-2) then
+      if temp=(max_size) then
         full_signal <= '1';
         empty_signal <= '0';
       elsif temp=0 then
@@ -129,7 +105,7 @@ begin
         empty_signal <= '0';
       end if;
         
-    end if ;
-  end process ; -- POP
+    end if;
+  end process ;
 end Behavioral;
 
